d8444cd0711f9a9929b3e8899a3810f1bc8ff546
wufa2023 <150779173+wufa2023@users.noreply.github.com>
2026-01-14 12:37:47 +0800

    Delete Exp_Code directory

--- Exp_Code/CC_Effect_Remove/CC_Effect_CCPE_Run.m
+++
@@ -1,8 +0,0 @@
-addpath(genpath('drtoolbox'));
-load('data/hNSC_preprocessed.mat')
-lambda=70;
-gamma=140;
-sigma=0.001;  %Gaussian distribution
-expression_matrix = double(expression_matrix)
-[pseudotime] = CCPE(expression_matrix,lambda,gamma,sigma);
-csvwrite("data/hNSC_pseudotime.csv", pseudotime);
